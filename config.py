cd1c219cf208d13881b7da2e535792ae9dab0bdf
zhenMoses <zhendeaini714107>
2018-12-28 17:51:33 +0800

    增加注释,把配置类剥离出去

---
+++ config.py
@@ -0,0 +1,49 @@
+from redis import StrictRedis
+
+# 0.创建配置类(父类)
+class Config(object):
+    """自定义配置类,将配置信息以属性的方式罗列即可"""
+
+    # mysql数据库配置信息
+    # 连接mysql数据库的配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
+    # 开启数据库跟踪模式
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # 使用session记得添加加密字符串对session_id进行加密处理
+    SECRET_KEY = "HFDSEUUHSKH$#*(UIWOhdsj&^fhsue4jfi6se&*^jiJFDKJSIECVMNBQER"
+
+    # redis数据库配置信息
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+
+    # 将flask中session存储到redis数据库的配置信息
+    # 存储到那种类型的数据库
+    SESSION_TYPE = 'redis'
+    # session存储的数据后产生的session_id需要加密
+    SESSION_USE_SIGNE = True
+    # 具体将session中的数据存储到哪个redis数据库对象
+    SESSION_REDIS=StrictRedis(host=REDIS_HOST, port=REDIS_PORT,db=1)
+    # 设置非永久存储
+    SESSION_PERMANENT = False
+    # 设置过期时长,默认过期时长:31天
+    PERMANENT_SESSION_LIFETIME = 86400
+
+
+class DevelopmentConfig(Config):
+    """开发模式的配置类"""
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """线上模式的配置类"""
+    DEBUG = False
+#     设置日志级别
+
+
+# 给外界使用提供一个接口
+# 使用:config_dict["development"] --->DevelopmentConfig
+config_dict = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig
+}
--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask import Flask,session
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from redis import StrictRedis
@@ -6,39 +6,51 @@ from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
+from config import config_dict
 
-class Config(object):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-    SECRET_KEY = "HFDSEUUHSKH$#*(UIWOhdsj&^fhsue4jfi6se&*^jiJFDKJSIECVMNBQER"
-
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    SESSION_TYPE = 'redis'
-    SESSION_USE_SIGNE = True
-    SESSION_REDIS=StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    PERMANENT_SESSION_LIFETIME = 86400
-
-
+#  1.创建app对象
 app = Flask(__name__)
+# 根据development健获取对应的配置类名
+config_class = config_dict['development']
+# DevelopmentConfig ---> 开发模式的app对象
+# ProductionConfig --->  线上模式的app对象
+app.config.from_object(config_class)
 
-app.config.from_object(Config)
-
+# 2.创建mysql数据库对象
 db = SQLAlchemy(app)
 
+# 3.创建redis数据库对象
+redis_store = StrictRedis(host=config_class.REDIS_HOST, port=config_class.REDIS_PORT)
+
+"""
+   redis_store.set("age", 18)  ---->存储到redis ---0号数据库
+   session["name"] = "laowang" ---->存储到redis ---1号数据库
+   """
+# 4.开启后端的CSRF保护机制
+"""
+# 底层：
+# 1.提取cookie中的csrf_token的值
+# 2.提取表单中的csrf_token的值，或者ajax请求的头中的X-CSRFToken键对应的值
+# 3.对比这两个值是否相等
+"""
 CSRFProtect(app)
 
 
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 5.借助Session调整flask.session的存储位置到redis中存储
+Session(app)
 
+
+#6. 创建数据管理对象,将app 交给管理对象管理
 manager = Manager(app)
+#  7.数据库迁移初始化
 Migrate(app, db)
+# 8.添加迁移命令
 manager.add_command('db', MigrateCommand)
 
-Session(app)
+
+
+
+
 @app.route('/')
 def index():
     return "hello world"
@@ -48,4 +60,5 @@ def index():
 
 
 if  __name__ =='__main__':
+       # 9.使用manager对象启动flask项目,代替app.run()
        manager.run()
