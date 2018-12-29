f3e70e2cfe83c6dd17392ace3fcffb80cf6db727
zhenMoses <zhendeaini714107>
2018-12-29 17:01:05 +0800

    设置根目录直接访问一个页面

--- info/modules/index/__init__.py
+++ info/modules/index/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
 
-index_bp=Blueprint("index",__name__)
+index_bp=Blueprint("index",__name__,static_folder="../../static/news",static_url_path="")
 from info.modules.index.views import *
--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,22 +1,22 @@
 from info.modules.index import index_bp
 import logging
 from flask import current_app
-
+from flask import  render_template
 
 @index_bp.route('/')
 def index():
     from info import redis_store
     redis_store.set("name", "laowang")
 
-    # 日志使用
-    logging.debug("This is a debug log.")
-    logging.info("This is a info log.")
-    logging.warning("This is a warning log.")
-    logging.error("This is a error log.")
-    logging.critical("This is a critical log.")
+    # # 日志使用
+    # logging.debug("This is a debug log.")
+    # logging.info("This is a info log.")
+    # logging.warning("This is a warning log.")
+    # logging.error("This is a error log.")
+    # logging.critical("This is a critical log.")
 
 
     # 另外一种记录日志的方法
     # flask中记录日志方法（项目采用这种方式记录）
     current_app.logger.debug("flask记录debug信息")
-    return "index"
+    return render_template("news/index.html")
