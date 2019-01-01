f4b153b37c3d001a1404e91b67a7524464c26abb
zhenMoses <zhendeaini714107>
2019-01-01 12:51:47 +0800

    查询新闻分类数据的前后端实现

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -2,7 +2,7 @@ from info.modules.index import index_bp
 import logging
 from flask import current_app, session, jsonify
 from flask import  render_template
-from info.models import User,News
+from info.models import User, News, Category
 from info.response_code import RET
 from info import constants
 
@@ -60,10 +60,25 @@ def index():
     for news in news_rank_list if news_rank_list else []:
         news_list_dict.append(news.to_dict())
 
+
+# ---------------3.查询新闻分类数据展示----------------
+#     categories:[新闻分类列表]
+    try:
+        categories=Category.query.all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="查询分类数据异常")
+
+    category_dict_list=[]
+    for category in categories if categories else []:
+        category_dict_list.append(category.to_dict())
+
+
     # 4.组织响应数据
     data = {
         "user_info": user_dict,
-        "chick_news_list": news_list_dict
+        "chick_news_list": news_list_dict,
+        "categories":category_dict_list
     }
     # 5.返回渲染页面
     return render_template("news/index.html", data=data)
