8039303d1dcdcf698ac305b3018f6dfe488cf36d
zhenMoses <zhendeaini714107>
2019-01-02 17:09:09 +0800

    实现基类模板抽取,继承模板

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,7 +1,11 @@
 from info.modules.news import news_bp
-from flask import render_template
+from flask import render_template, session
+
 
 @news_bp.route('/<int:news_id>')
 def news_detail(news_id):
+    """新闻详情页面展示"""
 
-    return render_template('news/detail.html')
+    data={}
+
+    return render_template('news/detail.html',data=data)
